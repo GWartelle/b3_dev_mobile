2b836e75473a185d1c98255b6576be2abc8e7c80
GWartelle <115559211+GWartelle@users.noreply.github.com>
2025-02-11 10:33:12 +0100

    Quiz home page implementation

---
+++ b3_quiz/lib/pages/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:b3_quiz/pages/question_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "The extreme quiz",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          elevation: 3,
+          backgroundColor: Colors.blue,
+          shadowColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                  child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://images.unsplash.com/photo-1517258307935-9764dad5d7de?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                    Text(
+                      "Are you ready to take on a challenge?!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => QuestionPage(),
+                              ));
+                        },
+                        child: Text("Try the extreme quiz!!!"))
+                  ],
+                ),
+              ))
+            ],
+          ),
+        )));
+  }
+}
